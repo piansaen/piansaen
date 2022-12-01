7575fc714aa8514e1f680476281f256f8a32ec05
piansaen <pian.python@gmail.com>
2022-12-01 16:12:09 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-print("Hello, welcome to my test python")
+#print("Hello, welcome to my test python")
 
-input("What is your name :")
-print ("sdlfjksd;fsjkd")
+#input("What is your name :")
+#print ("sdlfjksd;fsjkd")
+ 
+ #print ";sdfjs;dkjfs;fkjsd;"
+print("sdlkfjs;fjksd")
+
+
