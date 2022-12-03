d211c43130f48f17d77583ebcaa6e6c125c3c7db
piansaen <pian.python@gmail.com>
2022-12-03 10:19:06 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,9 @@
 #input("What is your name :")
 #print ("sdlfjksd;fsjkd")
  
- #print ";sdfjs;dkjfs;fkjsd;"
-print("sdlkfjs;fjksd")
+ #print ";sdfjs;dkjfs;fkjsd;"\
+name = input("Hello, what is your name?: ")
+age = input("How old are you :")
 
-
+print ("Nice to meet you ",name)
+print ("you are , ",age ," years old.")
